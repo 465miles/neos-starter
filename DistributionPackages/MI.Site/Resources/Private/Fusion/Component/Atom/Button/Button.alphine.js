9044df9b361a535a2a6810e2058ad5355892f41a
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-01 15:31:16 +0100

    REFACTOR: Cleanup Components

--- DistributionPackages/MI.Site/Resources/Private/Fusion/Component/Atom/Button/Button.alphine.js
+++ DistributionPackages/MI.Site/Resources/Private/Fusion/Component/Atom/Button/Button.alphine.js
@@ -2,7 +2,9 @@
 export default Alpine => {
 
     Alpine.data('MI_Site_Component_Atom_Button', () => ({
-        foo: () => console.log("foo")
+        foo() {
+            console.log("foo")
+        }
     }))
 
 }

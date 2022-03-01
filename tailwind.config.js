c615087b9ce3f55104679590f887d063b8af7b58
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-01 12:33:11 +0100

    REFACTOR: Generate symmetric css grid template columns via helper functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const content = require("./Build/Carbon.Pipeline/purge");
+const content = require('./Build/Carbon.Pipeline/purge');
 
 module.exports = {
     content,
@@ -8,13 +8,101 @@ module.exports = {
                 'sans': ['Outfit', 'ui-sans-serif', 'system-ui'],
                 'body': ['Outfit', 'ui-sans-serif', 'system-ui'],
             },
-            gridTemplateColumns: (theme) => ({
-                'container-xs': '[page-start] ' + theme("spacing.5") + ' [main-start large-start medium-start small-start] ' + theme("spacing.5") + ' [large-inner-start medium-inner-start small-inner-start] auto [center-left] ' + theme("spacing[2.5]") + ' [center] ' + theme("spacing[2.5]") + ' [center-right] auto [small-inner-end medium-inner-end large-inner-end] ' + theme("spacing.5") + ' [small-end medium-end large-end main-end] ' + theme("spacing.5") + ' [page-end]',
-                'container-sm': '[page-start] ' + theme("spacing.5") + ' [main-start large-start medium-start] ' + theme("spacing.5") + ' [large-inner-start medium-inner-start] ' + theme("spacing.5") + ' [small-start] ' + theme("spacing.5") + ' [small-inner-start] auto [center-left] ' + theme("spacing[2.5]") + '[center] ' + theme("spacing[2.5]") + ' [center-right] auto [small-inner-end] ' + theme("spacing.5") + ' [small-end] ' + theme("spacing.5") + ' [medium-inner-end large-inner-end] ' + theme("spacing.5") + ' [medium-end large-end main-end] ' + theme("spacing.5") + ' [page-end]',
-                'container-md': '[page-start] ' + theme("spacing.10") + ' [main-start large-start] ' + theme("spacing.10") + ' [large-inner-start] 1fr [medium-start] ' + theme("spacing.10") + ' [medium-inner-start small-start] ' + theme("spacing.10") + ' [small-inner-start] 5fr [center-left] ' + theme("spacing.5") + '[center] ' + theme("spacing.5") + ' [center-right] 5fr [small-inner-end] ' + theme("spacing.10") + ' [small-end medium-inner-end] ' + theme("spacing.10") + ' [medium-end] 1fr [large-inner-end] ' + theme("spacing.10") + ' [large-end main-end] ' + theme("spacing.10") + ' [page-end]',
-                'container-lg': '[page-start] ' + theme("spacing.10") + ' [main-start] 1fr [large-start] ' + theme("spacing.10") + ' [large-inner-start] 1fr [medium-start] ' + theme("spacing.10") + ' [medium-inner-start] 1fr [small-start] ' + theme("spacing.10") + ' [small-inner-start] 4fr [center-left] ' + theme("spacing.5") + '[center] ' + theme("spacing.5") + ' [center-right] 4fr [small-inner-end] ' + theme("spacing.10") + ' [small-end] 1fr [medium-inner-end] ' + theme("spacing.10") + ' [medium-end] 1fr [large-inner-end] ' + theme("spacing.10") + ' [large-end] 1fr [main-end] ' + theme("spacing.10") + ' [page-end]',
-                'container-2xl': '[page-start] ' + theme("spacing.10") + ' [main-start] 2fr [large-start] ' + theme("spacing.10") + ' [large-inner-start] 1fr [medium-start] ' + theme("spacing.10") + ' [medium-inner-start] 1fr [small-start] ' + theme("spacing.10") + ' [small-inner-start] 3fr [center-left] ' + theme("spacing.5") + '[center] ' + theme("spacing.5") + ' [center-right] 3fr [small-inner-end] ' + theme("spacing.10") + ' [small-end] 1fr [medium-inner-end] ' + theme("spacing.10") + ' [medium-end] 1fr [large-inner-end] ' + theme("spacing.10") + ' [large-end] 2fr [main-end] ' + theme("spacing.10") + ' [page-end]',
-            }),
+            gridTemplateColumns: (theme) => {
+
+                const area = (name, spacing, nested) => {
+                    const areaNames = name.split(' ')
+                    const startName = areaNames.map(name => `${name}-start`).join(' ')
+                    const endName = areaNames.reverse().map(name => `${name}-end`).join(' ')
+                    return `[${startName}] ${spacing} ${nested} ${spacing} [${endName}]`;
+                }
+
+                const center = spacing => `[center-left] ${spacing} [center] ${spacing} [center-right]`
+
+                return {
+
+                    'container-xs'
+                :
+                    area('page', theme('spacing.5'),
+                        area('main large medium small', theme('spacing.5'),
+                            area('large-inner medium-inner small-inner', 'auto',
+                                center(theme('spacing[2.5]'))
+                            )
+                        )
+                    ),
+
+                    'container-sm'
+                :
+                    area('page', theme('spacing.5'),
+                        area('main large medium', theme('spacing.5'),
+                            area('large-inner medium-inner', theme('spacing.5'),
+                                area('small', theme('spacing.5'),
+                                    area('small-inner', 'auto',
+                                        center(theme('spacing[2.5]'))
+                                    )
+                                )
+                            )
+                        )
+                    ),
+
+                    'container-md'
+                :
+                    area('page', theme('spacing.10'),
+                        area('main large', theme('spacing.10'),
+                            area('large-inner', '1fr',
+                                area('medium', theme('spacing.10'),
+                                    area('medium-inner small', theme('spacing.10'),
+                                        area('small-inner', '5fr',
+                                            center(theme('spacing.5'))
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ),
+
+                    'container-lg'
+                :
+                    area('page', theme('spacing.10'),
+                        area('main', '1fr',
+                            area('large', theme('spacing.10'),
+                                area('large-inner', '1fr',
+                                    area('medium', theme('spacing.10'),
+                                        area('medium-inner', '1fr',
+                                            area('small', theme('spacing.10'),
+                                                area('small-inner', '4fr',
+                                                    center(theme('spacing.5'))
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    ),
+
+                    'container-2xl'
+                :
+                    area('page', theme('spacing.10'),
+                        area('main', '2fr',
+                            area('large', theme('spacing.10'),
+                                area('large-inner', '1fr',
+                                    area('medium', theme('spacing.10'),
+                                        area('medium-inner', '1fr',
+                                            area('small', theme('spacing.10'),
+                                                area('small-inner', '3fr',
+                                                    center(theme('spacing.5'))
+                                                )
+                                            )
+                                        )
+                                    )
+                                )
+                            )
+                        )
+                    )
+                }
+            }
+            ,
             typography: (theme) => ({
                 DEFAULT: {
                     css: {

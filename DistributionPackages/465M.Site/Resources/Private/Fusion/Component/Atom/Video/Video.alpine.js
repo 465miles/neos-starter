11f6b5d159386f38ccb978784d4c7d49e1b70153
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-01 12:32:20 +0100

    REFACTOR: Alpine js components use `this` in int

--- DistributionPackages/465M.Site/Resources/Private/Fusion/Component/Atom/Video/Video.alpine.js
+++ DistributionPackages/465M.Site/Resources/Private/Fusion/Component/Atom/Video/Video.alpine.js
@@ -2,6 +2,8 @@ import Plyr from 'plyr';
 
 export default Alpine => (
     Alpine.data('MI_Site_Component_Atom_Video', () => ({
-        init: ($el) => new Plyr($el)
+        init() {
+            new Plyr(this.$el)
+        }
     }))
 )
--- DistributionPackages/465M.Site/Resources/Private/Fusion/Component/Atom/VideoExternal/VideoExternal.alpine.js
+++ DistributionPackages/465M.Site/Resources/Private/Fusion/Component/Atom/VideoExternal/VideoExternal.alpine.js
@@ -2,10 +2,12 @@ import Plyr from 'plyr';
 
 export default Alpine => {
     Alpine.data('MI_Site_Component_Atom_VideoExternal', () => ({
-        init: ($el) => new Plyr($el, {
-            youtube: {
-                noCookie: true,
-            },
-        })
+        init() {
+            new Plyr(this.$el, {
+                youtube: {
+                    noCookie: true,
+                },
+            })
+        }
     }))
 }

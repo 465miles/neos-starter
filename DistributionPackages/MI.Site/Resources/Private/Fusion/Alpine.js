5c823c92441dbcfe9bfc785f4504fd5b16c65271
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-02 16:49:13 +0100

    Bugfix: Try to fix accordion backend via x-collapse

--- DistributionPackages/MI.Site/Resources/Private/Fusion/Alpine.js
+++ DistributionPackages/MI.Site/Resources/Private/Fusion/Alpine.js
@@ -1,8 +1,11 @@
 import Alpine from 'alpinejs'
+import collapse from '@alpinejs/collapse'
 
 import Button from './Component/Atom/Button/Button.alphine'
 import Video from './Component/Atom/Video/Video.alpine'
 import VideoExternal from './Component/Atom/VideoExternal/VideoExternal.alpine'
+
+Alpine.plugin(collapse)
 
 Button(Alpine)
 Video(Alpine)

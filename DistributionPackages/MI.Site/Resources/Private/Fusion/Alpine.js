31a316ce2e5ca593372fb52d1128f83b06651494
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2022-03-02 23:40:34 +0100

    Refactor to AccordionItem.alpine.js

--- DistributionPackages/MI.Site/Resources/Private/Fusion/Alpine.js
+++ DistributionPackages/MI.Site/Resources/Private/Fusion/Alpine.js
@@ -4,11 +4,13 @@ import collapse from '@alpinejs/collapse'
 import Button from './Component/Atom/Button/Button.alphine'
 import Video from './Component/Atom/Video/Video.alpine'
 import VideoExternal from './Component/Atom/VideoExternal/VideoExternal.alpine'
+import AccordionItem from './Component/Atom/AccordionItem/AccordionItem.alpine'
 
 Alpine.plugin(collapse)
 
 Button(Alpine)
 Video(Alpine)
 VideoExternal(Alpine)
+AccordionItem(Alpine)
 
 Alpine.start()
